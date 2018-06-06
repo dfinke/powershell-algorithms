7b8f5a8396f5cf1fc03f439f5f292b6f098da5fc
Doug Finke <finked@hotmail.com>
2018-06-06 16:15:56 -0400

    Create cartesianProduct.ps1

---
+++ src/algorithms/sets/cartesian-product/cartesianProduct.ps1
@@ -0,0 +1,15 @@
+function cartesianProduct([object[]]$setA, [object[]]$setB) {
+    if (!$setA -or !$setB -or !$setA.length -or !$setB.length) {
+        return $null
+    }
+
+    $product = @()
+
+    for ($indexA = 0; $indexA -lt $setA.length; $indexA += 1) {
+        for ($indexB = 0; $indexB -lt $setB.length; $indexB += 1) {     
+            $product += $setA[$indexA], $setB[$indexB]
+        }
+    }
+
+    $product
+}
