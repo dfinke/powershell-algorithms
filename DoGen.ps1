d31e0ad7c84d8a99c79190e1c16b11ff44e56a97
dfinke <finked@hotmail.com>
2018-06-10 18:03:58 -0400

    added

---
+++ DoGen.ps1
@@ -0,0 +1,24 @@
+﻿function dogen {
+    param(
+        [Parameter(Mandatory)]    
+        $name
+    )
+
+
+$fileName  = ($name -split '-' | % {$_.substring(0,1).toupper()+$_.substring(1)}) -join ''
+
+pushd
+md $name
+cd $name
+
+md __test__
+pushd
+cd __test__
+"" | set-content "$($filename).tests.ps1"
+popd
+"" | set-content "README.md"
+"" | set-content "$($filename).ps1"
+
+}
+
+#dogen knuth-morris-pratt
