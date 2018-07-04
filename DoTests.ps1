ed62dd1f81a667051a40bfa91ca819ee94402d4d
dfinke <finked@hotmail.com>
2018-07-04 09:24:45 -0400

    report failed tests in appveyor

--- DoTests.ps1
+++ DoTests.ps1
@@ -4,4 +4,8 @@ if ((Get-Module -ListAvailable pester) -eq $null) {
     Install-Module -Name Pester -Repository PSGallery -Force
 }
 
-Invoke-Pester -Script $PSScriptRoot
+$result = Invoke-Pester -Script $PSScriptRoot -Verbose -PassThru
+
+if ($result.FailedCount -gt 0) {
+    throw "$($result.FailedCount) tests failed."
+}
