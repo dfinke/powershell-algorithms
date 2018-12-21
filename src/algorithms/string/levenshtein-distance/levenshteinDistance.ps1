d9c987e20b6756467fa3ac52cdf533c95c6a9943
dfinke <finked@hotmail.com>
2018-12-21 16:25:02 -0500

    added comments

--- src/algorithms/string/levenshtein-distance/levenshteinDistance.ps1
+++ src/algorithms/string/levenshtein-distance/levenshteinDistance.ps1
@@ -1,11 +1,19 @@
 function levenshteinDistance([string]$a, [string]$b) {
 
+    # Create empty edit distance matrix for all possible modifications of
+    # substrings of a to substrings of b.
     $distanceMatrix = (0..($b.Length + 1)).ForEach( {New-Object object[] ($a.Length + 1)} )
 
+    # Fill the first row of the matrix.
+    # If this is first row then we're transforming empty string to a.
+    # In this case the number of transformations equals to size of a substring.
     for ($i = 0; $i -le $a.length; $i += 1) {
         $distanceMatrix[0][$i] = $i
     }
 
+    # Fill the first column of the matrix.
+    # If this is first column then we're transforming empty string to b.
+    # In this case the number of transformations equals to size of b substring.
     for ($j = 0; $j -le $b.length; $j += 1) {
         $distanceMatrix[$j][0] = $j;
     }
